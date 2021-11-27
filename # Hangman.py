d963602027f0c772d6d85d0079506e9bb62f7cf6
asad-ullah-habib <ullah4asad@gmail.com>
2021-11-27 22:22:20 +0500

    Done with the program. Integration with gui toolkit remains. Completed problem of multiple instances of the same character in variable word.

--- # Hangman.py
+++ # Hangman.py
@@ -1,13 +1,14 @@
 import random
 
 # variables
+from typing import List
+
 max_tries = 3
 max_errors = 3
 list_of_words = ['FIVEE', 'FOUR', 'THR', 'SIXXXX', 'A']
-word = list_of_words[random.randint(0, len(list_of_words) - 1)]  # to choose word
-chars_in_word = ['_'] * (len(word))
 guessed = []  # list of guessed words
-list_of_letters = ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth']
+list_of_letters = ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh',
+                   'Eighth']  # needed for print statement
 
 
 def list_to_string(s):  # helper function( just hide )
@@ -17,7 +18,7 @@ def list_to_string(s):  # helper function( just hide )
     return str1
 
 
-def initialise():  # a welcome script. Ignore
+def initialise(word):  # a welcome script. Ignore
     print('Welcome to Hangman!' + '\n' + 'You have', max_tries, 'available lives')
     print('Your word has', len(word), 'letters')
     print('_' * len(word))
@@ -27,10 +28,11 @@ def main_program():
     user_tries = 0
     error_counter = 0
     counter_for_letter = 0
-    initialise()
+    word = list_of_words[random.randint(0, len(list_of_words) - 1)]  # to choose word
+    chars_in_word = ['_'] * (len(word)) # used to display the words user got right
+    initialise(word) # program starts
 
     while user_tries <= max_tries - 1:
-
         print('Enter your', list_of_letters[counter_for_letter], 'letter: ')  # Taking Input
         user_try = input()
 
@@ -38,16 +40,20 @@ def main_program():
             user_try = user_try.upper()  # capitalising
 
             if user_try.upper() in word:  # if guess is correct
-                print('Sucess')
+                print('Correct!')
                 counter_for_letter += 1
+                func_word = word  # made for use in the for loop below (can be ignored)
 
-                # need to add code to handle multiple instances of the same character #
+                for i in range(len(word)):
+                    if func_word.find(user_try.upper()) == -1:  # check if char is not in word
+                        break
+                    else:
+                        chars_in_word.pop(func_word.find(user_try.upper()))  # remove char from list
+                        chars_in_word.insert(func_word.find(user_try.upper()), user_try.upper())  # add user_try to list
+                        func_word = func_word.replace(user_try.upper(), str(i), 1)  # modify func_word
+                print(list_to_string(chars_in_word))  # print letters user got right
 
-                chars_in_word.pop(word.find(user_try.upper()))
-                chars_in_word.insert(word.find(user_try.upper()), user_try.upper())
-                print(list_to_string(chars_in_word))
-
-                if word == list_to_string(chars_in_word):
+                if word == list_to_string(chars_in_word): # if user wins
                     print('You won!. Congrats')
                     break
 
@@ -58,21 +64,12 @@ def main_program():
                 else:
                     if user_tries < max_tries - 1:
                         print('Fail! Try Again')
-                    guessed.append(user_try.upper())
+                    guessed.append(user_try.upper())  # make a list of words user has guessed
                     user_tries += 1
-
-            elif len(word) == len(chars_in_word):
-                print('You won!')
-                break
-
-            elif error_counter > max_errors:
-                print('Game Over')
-                break
 
         else:
             print('Value Error. Enter a Valid Value.')
             error_counter += 1
-
             if error_counter > max_errors:
                 print('Too many Errors!. Game over')
                 break
