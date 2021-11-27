cc2dd991bfa42829494e944c6d15590fc1dbee68
asad-ullah-habib <ullah4asad@gmail.com>
2021-11-27 21:25:21 +0500

    Modified print staement, made new list

--- # Hangman.py
+++ # Hangman.py
@@ -2,22 +2,22 @@ import random
 
 # variables
 max_tries = 3
+max_errors = 3
 list_of_words = ['FIVEE', 'FOUR', 'THR', 'SIXXXX', 'A']
-tries = 0
 word = list_of_words[random.randint(0, len(list_of_words) - 1)]  # to choose word
 chars_in_word = ['_'] * (len(word))
 guessed = []  # list of guessed words
-max_errors = 3
+list_of_letters = ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth']
 
 
-def list_to_string(s):  # helper function
+def list_to_string(s):  # helper function( just hide )
     str1 = ""
     for ele in s:
         str1 += ele
     return str1
 
 
-def initialise():
+def initialise():  # a welcome script. Ignore
     print('Welcome to Hangman!' + '\n' + 'You have', max_tries, 'available lives')
     print('Your word has', len(word), 'letters')
     print('_' * len(word))
@@ -26,18 +26,20 @@ def initialise():
 def main_program():
     user_tries = 0
     error_counter = 0
+    counter_for_letter = 0
     initialise()
 
     while user_tries <= max_tries - 1:
 
-        user_try = input('Enter your first Letter: ')  # Taking Input
+        print('Enter your', list_of_letters[counter_for_letter], 'letter: ')  # Taking Input
+        user_try = input()
 
         if len(user_try) == 1 and user_try.isalpha() is True:  # checking input
             user_try = user_try.upper()  # capitalising
 
             if user_try.upper() in word:  # if guess is correct
                 print('Sucess')
-                
+                counter_for_letter += 1
 
                 # need to add code to handle multiple instances of the same character #
 
@@ -74,7 +76,6 @@ def main_program():
             if error_counter > max_errors:
                 print('Too many Errors!. Game over')
                 break
-    # print('You lost!')
 
 
 main_program()
