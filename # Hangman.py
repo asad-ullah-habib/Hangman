e8e5a08e8582d4a6dddb07224a655fae0808175d
asad-ullah-habib <ullah4asad@gmail.com>
2021-11-28 00:49:42 +0500

    Some changes + new project

--- # Hangman.py
+++ # Hangman.py
@@ -8,7 +8,7 @@ for i in range(len(list_of_words)):  # capitalise all list
     list_of_words[i] = list_of_words[i].upper()
 guessed = []  # list of guessed words
 list_of_letters = ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh',
-                   'Eighth']  # needed for print statement
+                   'Eighth']  # needed for print statement, assuming max characters in a word are eight
 
 
 def list_to_string(s):  # helper function( just hide )
@@ -30,6 +30,7 @@ def main_program():
     counter_for_letter = 0
     word = list_of_words[random.randint(0, len(list_of_words) - 1)]  # to choose word
     chars_in_word = ['_'] * (len(word))  # used to display the words user got right
+
     initialise(word)  # program starts
 
     while user_tries <= max_tries - 1:
@@ -44,20 +45,20 @@ def main_program():
                 counter_for_letter += 1
                 func_word = word  # made for use in the for loop below (can be ignored)
 
-                for i in range(len(word)):
+                for u in range(len(word)):
                     if func_word.find(user_try) == -1:  # check if char is not in word
                         break
                     else:
                         chars_in_word.pop(func_word.find(user_try))  # remove char from list
                         chars_in_word.insert(func_word.find(user_try.upper()), user_try)  # add user_try to list
-                        func_word = func_word.replace(user_try, str(i), 1)  # modify func_word
+                        func_word = func_word.replace(user_try, str(u), 1)  # modify func_word
                 print(list_to_string(chars_in_word))  # print letters user got right
 
                 if word == list_to_string(chars_in_word):  # if user wins
                     print('You won!. Congrats')
                     break
 
-            elif user_try not in word:
+            elif user_try not in word:  # if guess is incorrect
                 if user_try in guessed:
                     print('You have already guessed this!')
 
