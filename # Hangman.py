1797e72f7751925dd0f5fccf6d43c90cd64d8e65
asad-ullah-habib <ullah4asad@gmail.com>
2021-11-30 21:46:07 +0500

    No bugs found. Added slow_print in all useful print statements. When using terminal, first enter env python3 and then the file path

--- # Hangman.py
+++ # Hangman.py
@@ -45,27 +45,28 @@ hangman_stages = ['''
   /|\  |
   / \  |
       ===''']
+
+
 # pictionary of hangman (minimised)
 
 
 def choose_a_word(dictionary):
-    master_dict = dictionary
-    for k in master_dict:  # capitalise every value in dictionary
-        for i in range(len(master_dict[k])):  # capitalise all list
-            master_dict[k][i] = master_dict[k][i].upper()
+    for k in dictionary:  # capitalise every value in dictionary
+        for i in range(len(dictionary[k])):  # capitalise all list
+            dictionary[k][i] = dictionary[k][i].upper()
 
     # to choose a random key(category) and value(word within that category)
     global word_key_value
-    word_key_value = random.choice(list(master_dict.keys()))
-    word_index_value = random.randint(0, len(master_dict[word_key_value]) - 1)
-    return master_dict[word_key_value][word_index_value]
+    word_key_value = random.choice(list(dictionary.keys()))
+    word_index_value = random.randint(0, len(dictionary[word_key_value]) - 1)
+    return dictionary[word_key_value][word_index_value]
 
 
 def slow_print(t):
     for i in t:
         sys.stdout.write(i)
         sys.stdout.flush()
-        time.sleep(0)  # change later #
+        time.sleep(0.02)  # change later #
     print('')
 
 
@@ -79,18 +80,20 @@ def list_to_string(s):  # helper function( just hide )
 def initialise(w):  # a welcome script. Ignore
 
     slow_print('Welcome to Hangman! \nYou have 7 available lives by default.')
-    print('Your word has', len(w), 'letters\n Your word is in the category:', word_key_value)
-    print('_' * len(w))
+    slow_print('Your word has ' + str(len(w)) + ' letters\nYour word is in the category: ' + str(word_key_value))
+    slow_print('_' * len(w))
 
 
 def difficulty():
     while True:
-        ask = input('Do you want to choose a difficulty level? [y/n]:')
+        slow_print('Do you want to choose a difficulty level? [y/n]')
+        ask = input()
         if len(ask) == 1 and ask.isalpha() is True:  # checking input
             ask = ask.lower()
             if ask == 'y':
                 for i in range(max_errors + 1):
-                    hangman_difficulty = input('Choose your difficulty: Easy[ 1 ], Medium[ 2 ] or Hard [ 3 ]: ')
+                    slow_print('Choose your difficulty: Easy[ 1 ], Medium[ 2 ] or Hard [ 3 ]: ')
+                    hangman_difficulty = input()
                     if len(hangman_difficulty) == 1 and hangman_difficulty.isnumeric() is True:
                         if hangman_difficulty == '1':
                             return 7
@@ -106,27 +109,21 @@ def difficulty():
 
         else:
             print('Input Error. Enter a Valid Value.'
-                  '\nValue has to be either Y or N ')
+                  '\nValue has to be either y or n')
 
 
-def main_program(user_tries, word,  difficulty_level):
+def main_program(user_tries, word, difficulty_level):
     error_counter = 0
     counter_for_letter = 0
-    try:
-        max_tries = difficulty_level
-
-    except TypeError:
-        print('Input Error. Enter a Valid Value.'
-              '\nValue has to be either Y or N ')
-
+    max_tries = difficulty_level
     while user_tries <= max_tries - 1:
-        user_try = input('Try to guess your ' + str(list_of_letters[counter_for_letter]) + ' letter: ')
-
+        slow_print('Try to guess your ' + str(list_of_letters[counter_for_letter]) + ' letter: ')
+        user_try = input()
 
         if len(user_try) == 1 and user_try.isalpha() is True:  # checking input
             user_try = user_try.upper()  # capitalising
 
-            if user_try in word:  # if guess is correct
+            if user_try in word and user_try not in guessed:  # if guess is correct
                 print('You are Correct!')
                 counter_for_letter += 1
                 func_word = word  # made for use in the for loop below (can be ignored)
@@ -138,23 +135,23 @@ def main_program(user_tries, word,  difficulty_level):
                         chars_in_word.pop(func_word.find(user_try))  # remove char from list
                         chars_in_word.insert(func_word.find(user_try.upper()), user_try)  # add user_try to list
                         func_word = func_word.replace(user_try, str(u), 1)  # modify func_word
-                print(list_to_string(chars_in_word))  # print letters user got right
+                slow_print(str(list_to_string(chars_in_word)))  # print letters user got right
 
                 if word == list_to_string(chars_in_word):  # if user wins. print win statement
-                    print('You won!. Congratulations!!!!')
+                    slow_print('You won!. Congratulations!!!!')
                     break
 
             elif user_try not in word:  # if guess is incorrect
                 if user_try in guessed:
-                    print('You have already guessed this!\nTry again.')
+                    slow_print('You have already guessed this!\nTry again.')
 
                 else:
                     if user_tries < max_tries - 1:
-                        print('Your guess is incorrect.\n Try Again')
+                        slow_print('Your guess is incorrect.\n Try Again')
                         print(hangman_stages[user_tries])
                     else:
-                        print('You have used up all your tries, sorry.'
-                              '\nGame Lost!')
+                        slow_print('You have used up all your tries, sorry.'
+                                   '\nGame Lost!')
                         print(hangman_stages[user_tries])
                     guessed.append(user_try)  # make a list of words user has guessed
                     user_tries += 1
